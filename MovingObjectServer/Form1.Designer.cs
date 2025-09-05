793189734e9bc1f5baa61927a0eb38d95c111015
Rashelyo Syawalian <168207952+Rashelyo123@users.noreply.github.com>
2025-09-05 20:38:39 +0700

    Add files via upload

---
+++ MovingObjectServer/Form1.Designer.cs
@@ -0,0 +1,37 @@
+﻿namespace MovingObject
+{
+    partial class Form1 : System.Windows.Forms.Form
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Timer timer1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+
+            // timer1
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 344);
+            this.Name = "Form1";
+            this.Text = "Server";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ MovingObjectServer/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MovingObject
+{
+    public partial class Form1 : Form
+    {
+        Pen red = new Pen(Color.Red);
+        Rectangle rect = new Rectangle(20, 20, 30, 30);
+        SolidBrush fillBlue = new SolidBrush(Color.Blue);
+        int slide = 10;
+
+        private TcpListener server;
+        private Thread serverThread;
+        private List<TcpClient> clients = new List<TcpClient>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 50;
+            timer1.Enabled = true;
+
+            StartServer();
+        }
+
+        private void StartServer()
+        {
+            serverThread = new Thread(() =>
+            {
+                try
+                {
+                    server = new TcpListener(IPAddress.Any, 5000);
+                    server.Start();
+                    while (true)
+                    {
+                        var client = server.AcceptTcpClient();
+                        lock (clients)
+                        {
+                            clients.Add(client);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Server error: " + ex.Message);
+                }
+            });
+            serverThread.IsBackground = true;
+            serverThread.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            back();
+            rect.X += slide;
+            Invalidate();
+
+            BroadcastPosition();
+        }
+
+        private void back()
+        {
+            if (rect.X >= this.Width - rect.Width * 2)
+                slide = -10;
+            else if (rect.X <= rect.Width / 2)
+                slide = 10;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawRectangle(red, rect);
+            g.FillRectangle(fillBlue, rect);
+        }
+
+        private void BroadcastPosition()
+        {
+            string message = $"{rect.X},{rect.Y}";
+            byte[] data = Encoding.UTF8.GetBytes(message);
+
+            lock (clients)
+            {
+                foreach (var client in clients)
+                {
+                    try
+                    {
+                        if (client.Connected)
+                        {
+                            var stream = client.GetStream();
+                            stream.Write(data, 0, data.Length);
+                        }
+                    }
+                    catch
+                    {
+                        // Jika client error, abaikan
+                    }
+                }
+            }
+        }
+    }
+}
